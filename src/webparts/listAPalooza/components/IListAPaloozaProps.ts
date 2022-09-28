d1737168744f90be36165dc1d2014fea99344546
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-09-28 00:42:34 -0400

    Initial Yo generated web part

---
+++ src/webparts/listAPalooza/components/IListAPaloozaProps.ts
@@ -0,0 +1,7 @@
+export interface IListAPaloozaProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/listAPalooza/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IListAPaloozaWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ListAPaloozaWebPartStrings' {
+  const strings: IListAPaloozaWebPartStrings;
+  export = strings;
+}
